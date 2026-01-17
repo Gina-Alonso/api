7cc0a6bf47618c1ffe42f997cf1a79145dea7407
Georgina Alonso <ginaoalonso.2603@gmail.com>
2026-01-17 17:46:14 -0600

    Manejo de errores 404 y 400 en pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,12 +2,15 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.pacientes.*;
+import med.voll.api.pacientes.DatosDetallePacienteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -16,26 +19,38 @@ public class PacienteController {
     PacienteRepository pacienteRepository;
     @Transactional
     @PostMapping
-    public void registrar(@RequestBody @Valid DatosRegistroPaciente datos) {
-        System.out.println("datos recibidos: " +datos);
-        pacienteRepository.save(new Paciente(datos));
+    public ResponseEntity registrar(@RequestBody @Valid DatosRegistroPaciente datos, UriComponentsBuilder uriComponentsBuilder) {
+        var paciente = new Paciente(datos);
+        pacienteRepository.save(paciente);
+        var uri = uriComponentsBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DatosDetallePacienteDTO(paciente));
     }
 
     @GetMapping
-    public Page<PacienteDTO> listar(@PageableDefault(page = 0, size = 10, sort = {"nombre"}) Pageable paginacion) {
-        return pacienteRepository.findAllByActivoTrue(paginacion).map(PacienteDTO::new);
+    public ResponseEntity<Page<PacienteDTO>> listar(@PageableDefault(page = 0, size = 10, sort = {"nombre"}) Pageable paginacion) {
+        var page = pacienteRepository.findAllByActivoTrue(paginacion).map(PacienteDTO::new);
+        return ResponseEntity.ok(page);
     }
     @PutMapping
     @Transactional
-    public void actualizar(@RequestBody @Valid PacienteDTOActualizado datos) {
+    public ResponseEntity actualizar(@RequestBody @Valid PacienteDTOActualizado datos) {
         var paciente = pacienteRepository.getReferenceById(datos.id());
         paciente.atualizarInformacion(datos);
+        return ResponseEntity.ok(new DatosDetallePacienteDTO(paciente));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void eliminar(@PathVariable Long id) {
+    public ResponseEntity eliminar(@PathVariable Long id) {
         var paciente = pacienteRepository.getReferenceById(id);
         paciente.desactivar();
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity detallar(@PathVariable Long id) {
+        var paciente = pacienteRepository.getReferenceById(id);
+        return ResponseEntity.ok(new DatosDetallePacienteDTO(paciente));
     }
 }
--- src/main/java/med/voll/api/pacientes/DatosDetallePacienteDTO.java
+++ src/main/java/med/voll/api/pacientes/DatosDetallePacienteDTO.java
@@ -1,4 +1,10 @@
 package med.voll.api.pacientes;
 
-public record DatosDetallePacienteDTO() {
+import med.voll.api.domain.direccion.Direccion;
+import med.voll.api.domain.pacientes.Paciente;
+
+public record DatosDetallePacienteDTO(String nombre, String email, String telefono, String documentoIdentidad, Direccion direccion) {
+    public DatosDetallePacienteDTO(Paciente paciente) {
+        this(paciente.getNombre(), paciente.getEmail(), paciente.getTelefono(), paciente.getDocumentoIdentidad(), paciente.getDireccion());
+    }
 }
