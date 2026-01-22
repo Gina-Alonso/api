276c8b599df7298b47486d038e5266680022d486
Georgina Alonso <ginaoalonso.2603@gmail.com>
2026-01-21 19:35:10 -0600

    Seguridad

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DatosAutenticacion;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DatosTokenJWTDTO;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,13 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AutenticacionController {
     @Autowired
+    private TokenService tokenService;
+    @Autowired
     private AuthenticationManager manager;
 
     @PostMapping
     public ResponseEntity iniciarSesion(@RequestBody @Valid DatosAutenticacion datos){
-        var token =  new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasenia());
-        var autenticacion = manager.authenticate(token);
-
-        return ResponseEntity.ok().build();
+        var authenticationToken =  new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasenia());
+        var autenticacion = manager.authenticate(authenticationToken);
+        var tokenJWT = tokenService.generarToken((Usuario) autenticacion.getPrincipal());
+        return ResponseEntity.ok(new DatosTokenJWTDTO(tokenJWT));
     }
 }
